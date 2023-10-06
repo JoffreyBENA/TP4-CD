357bbab35d235598193afa24cad819adcc4db364
Joffrey <117658241+JoffreyBENA@users.noreply.github.com>
2023-10-06 13:54:53 +0200

    update

--- brouillons/script-deployment/prepare_vm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Variables
-PROJECT_ID="<your_project_id>"
-ZONE="<your_zone>"
-VM_NAME="my-vm-instance"
-
-# SSH into the VM
-gcloud compute ssh $VM_NAME --project $PROJECT_ID --zone $ZONE --command "<your_prepare_commands>"
--- brouillons/terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-project_id = "<your_project_id>"
-region     = "<your_region>"
-zone       = "<your_zone>"
